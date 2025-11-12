6f271bba06b08e88d097275adaf199f964967e60
Kon <konstantin@stanmeyer.com>
2025-11-12 13:29:13 -0500

    initial v2 formatting

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="w-screen relative h-screen flex items-center justify-center">
-        {/* <img className="w-screen h-screen object-cover" src="/landing.png" /> */}
-        <video className="opacity-80 z-0 h-full w-full object-cover aspect-video" autoPlay muted loop id="myVideo">
-          <source src="Dscf0683.mp4" type="video/mp4"></source>
-        </video>
-        <p className="z-10 absolute zalando text-white text-5xl p-2">Stephen Jackson Smith</p>
+    <div className="w-screen h-screen flex flex-row">
+      <div className="w-3/5 bg-[#fefefc]">
+
+      </div>
+      <div className="w-2/5 bg-[#f54e00] flex items-center">
+        <div className=" flex flex-col items-center">
+          <div className="">
+
+          </div>
+          <img className="" />
+        </div>
       </div>
     </div>
   );
